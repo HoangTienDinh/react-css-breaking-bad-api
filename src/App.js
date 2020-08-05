72653f0c7b7715573312785c4d0bdfe6e441b03c
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-05 09:55:05 -0700

    add CharacterGrid component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Header from './components/ui/Header';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import axios from "axios";
 
 function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-
-
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
 
-      setItems(result.data)
-      setIsLoading(false)
-    }
+      setItems(result.data);
+      setIsLoading(false);
+    };
 
-    fetchItems()
-  }, [])
-
+    fetchItems();
+  }, []);
 
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
